b79a81b5cac0b968ebfb88ba1abe3b4827c0be4e
fishygobwub <samt34@mst.edu>
2017-11-13 22:05:25 -0600

    Added delay slider and loading bar to GUI. Currently non-functioning.

--- source/cInspect/src/cinspect/GUI/GUI.java
+++ source/cInspect/src/cinspect/GUI/GUI.java
@@ -2,6 +2,8 @@ package cinspect.GUI;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -10,18 +12,17 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.control.TextArea;
-import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
 
 import java.util.List;
@@ -38,13 +39,17 @@ import cinspect.web.WebResource;
 import com.aquafx_project.AquaFx;
 
 public class GUI extends Application {
-	Pane textDisplayPane, textInputPane;
-	VBox optionsVBox, checkboxVBox;
+	Pane textDisplayPane, textInputPane, loadingPane;
+	VBox optionsVBox, checkboxVBox, delayBox;
+	HBox bottomBox;
 	TextField inputTextField;
 	CheckBox sqlCheck, rceCheck, lfiCheck, xssCheck, 
 			 rfiCheck, tsqlCheck, udrjsCheck, appdosCheck,
 			 phpinfoCheck, ccssnCheck;
 	Button runButton, stopButton, closeButton, minimizeButton, maximizeButton;
+	Slider delaySlider;
+	Label delayLabel;
+	ProgressBar loadingBar;
 	static TextArea text;
 	double xOffset, yOffset, textOffset = 5;
 	boolean maxToggle = false;
@@ -67,6 +72,7 @@ public class GUI extends Application {
 		
 		//Display instantiation
 		textDisplayPane = new Pane();
+		bottomBox = new HBox();
 		textInputPane = new Pane();
 		textInputPane.setPrefHeight(50);
 		checkboxVBox = new VBox();
@@ -77,16 +83,23 @@ public class GUI extends Application {
 		optionsVBox.setStyle("-fx-padding: 5");
 		optionsVBox.setSpacing(10);
 		optionsVBox.setPrefWidth(125);
+		delayBox = new VBox();
+		loadingPane = new Pane();
+		loadingPane.setStyle("-fx-padding: 10;");
 		
-		//Save default window size
+		//Save default window size for resizing purposes
 		oldW = mainScene.getWidth();
 		oldH = mainScene.getHeight();
 		
-		//Interactive objects instantiation
+		//Interactive objects instantiation------------------------------
+		
+		//Text Field
 		inputTextField = new TextField();
 		inputTextField.setPrefWidth(375-textOffset);
 		inputTextField.setLayoutX(textOffset);
 		inputTextField.setOnAction(new TextFieldHandler());
+		
+		//Checkboxes
 		sqlCheck = new CheckBox("SQL");
 		rceCheck = new CheckBox("RCE");
 		lfiCheck = new CheckBox("LFI");
@@ -97,10 +110,25 @@ public class GUI extends Application {
 		appdosCheck = new CheckBox("AppDoS");
 		phpinfoCheck = new CheckBox("Phpinfo");
 		ccssnCheck = new CheckBox("CC/SSN");
+		
+		//Buttons
 		runButton = new Button("Run Program");
 		runButton.setOnAction(new RunButtonHandler());
 		stopButton = new Button("STOP");
 		stopButton.setOnAction(new StopButtonHandler());
+		
+		//Delay Slider
+		delayLabel = new Label("Delay: 0 ms");
+		delaySlider = new Slider();
+		delaySlider.setMin(0);
+		delaySlider.setMax(100);
+		delaySlider.setValue(0);
+		delaySlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+            	delayLabel.setText("Delay: " + String.format("%.0f", new_val) + " ms");
+            	loadingBar.setProgress(new_val.doubleValue()/100);
+            }
+        });
 		
 		//Toolbar
 		closeButton = new Button("  ");
@@ -168,6 +196,11 @@ public class GUI extends Application {
             }
         });
 		
+        //Loading Bar
+        loadingBar = new ProgressBar();
+        loadingBar.setProgress(0);
+        loadingBar.setLayoutX(10);
+        
 		//Scrolling Text
 		text = new TextArea();
 		text.setEditable(false);
@@ -176,20 +209,22 @@ public class GUI extends Application {
 		text.setLayoutY(textOffset);
 		
 		//Add items to VBox
-		optionsVBox.getChildren().add(checkboxVBox);
+		optionsVBox.getChildren().addAll(checkboxVBox, delayBox, runButton, stopButton);
 		checkboxVBox.getChildren().addAll(sqlCheck, rceCheck, lfiCheck, xssCheck, 
-			 rfiCheck, tsqlCheck, udrjsCheck, appdosCheck, phpinfoCheck, ccssnCheck);
-		optionsVBox.getChildren().addAll(runButton, stopButton);
+				 rfiCheck, tsqlCheck, udrjsCheck, appdosCheck, phpinfoCheck, ccssnCheck);
+		delayBox.getChildren().addAll(delayLabel, delaySlider);
 		
 		//Add items to Bottom Pane
-		textInputPane.getChildren().add(inputTextField);
+		bottomBox.getChildren().addAll(textInputPane, loadingPane);
+		textInputPane.getChildren().addAll(inputTextField);
+		loadingPane.getChildren().addAll(loadingBar);
 		
 		//Add items to Center Pane
 		textDisplayPane.getChildren().add(text);
 		
 		//BorderPane layout
 		mainPane.setCenter(textDisplayPane);
-		mainPane.setBottom(textInputPane);
+		mainPane.setBottom(bottomBox);
 		mainPane.setRight(optionsVBox);
 		mainPane.setTop(toolbar);
 		
